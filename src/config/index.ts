29d62fa3a43e9f8e00f3b0f71c2a0cc64497ff5e
chiranjeev thomas <chiranjeevthomas@chiranjeevs-MacBook-Pro.local>
2024-12-15 02:06:40 +0530

    add new user creation in database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,10 @@
 import { config } from "dotenv";
 config();
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, MONGODB_URI } = process.env;
 
 export const Config = {
-    PORT,
-    NODE_ENV,
+  PORT,
+  NODE_ENV,
+  MONGODB_URI,
 };
