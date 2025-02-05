00804d5e6206cbc0fea5b1779bb70aafc5b24282
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2025-02-05 18:43:39 +0530

    change the validation error parser to a more generic version

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const validationErrorParser = (result: any) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
